be4d9d86154c745d217abea500edcdfc6f8f18fd
Jeemin Shim <jeemin.shim@gmail.com>
2020-01-08 00:31:34 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,6 +7,6 @@ def run_guessing_game
   elsif input == "exit"
     puts "Goodbye!"
   else
-    puts "Sorry! The computer guessed <number>."
+    puts "Sorry! The computer guessed #{num}."
   end
 end
