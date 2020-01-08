8d6fcf527878914070f4890bba9fd01515e486c6
Jeemin Shim <jeemin.shim@gmail.com>
2020-01-08 00:31:04 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game
   puts "Guess a number between 1 and 6:"
   input = gets.chomp
     puts "You guessed the correct number!"
-  elsif input.downcase == "exit"
+  elsif input == "exit"
     puts "Goodbye!"
   end
 end
